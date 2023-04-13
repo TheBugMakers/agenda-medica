bca72f65ae5109577007b48e7ef881294cef7c7a
Felipe Roberto <56833987+feliperobertosouz@users.noreply.github.com>
2023-04-13 14:51:47 -0300

    add leftMenu component, add leftmenu component in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <v-app>
     <v-main class="grey lighten-3" id="container">
+      <LeftMenu></LeftMenu>
       <AdminTemplate v-if="user.data"></AdminTemplate>
       <PublicTemplate v-if="!user.data"></PublicTemplate>
     </v-main>
@@ -10,12 +11,14 @@
 <script>
 import AdminTemplate from "@/components/AdminTemplate"
 import PublicTemplate from "@/components/PublicTemplate"
+import LeftMenu from "@/components/LeftMenu.vue"
 
 export default {
   name: 'App',
   components: {
     AdminTemplate,
     PublicTemplate,
+    LeftMenu,
   },
   data: () => ({}),
   computed:{
